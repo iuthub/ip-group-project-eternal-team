8bf3595beafd5401da1c24cb38e4f9c7ffce8020
SardorAlijanov <55909711+SardorAlijanov@users.noreply.github.com>
2020-05-06 02:37:09 +0500

    updated
    login and registration are replaced, since we want them to be in one page, added our logo

--- eternal_project/resources/views/partials/navbar.blade.php
+++ eternal_project/resources/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            Mum Buy
+            <img src="{{ asset('images/logo.png')}}" width="150px" height="100px">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
@@ -23,18 +23,7 @@
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
                 <!-- Authentication Links -->
-
-
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-                @else
+                    @auth
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }} <span class="caret"></span>
@@ -52,7 +41,7 @@
                             </form>
                         </div>
                     </li>
-                @endguest
+                    @endauth
             </ul>
         </div>
     </div>

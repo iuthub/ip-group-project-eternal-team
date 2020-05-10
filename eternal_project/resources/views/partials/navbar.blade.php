a30439140c7eb15291de7f57d0951ff6255176b7
Mukhammad <m.rustamkhonov@student.inha.uz>
2020-05-10 15:41:57 +0500

    navbar remake

--- eternal_project/resources/views/partials/navbar.blade.php
+++ eternal_project/resources/views/partials/navbar.blade.php
@@ -1,65 +1,117 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            Mum Buy
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+{{--    <div class="container">--}}
+{{--        <a class="navbar-brand" href="{{ url('/') }}">--}}
+{{--            Mum Buy--}}
+{{--        </a>--}}
+{{--        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">--}}
+{{--            <span class="navbar-toggler-icon"></span>--}}
+{{--        </button>--}}
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
+{{--        <div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
+{{--            <!-- Left Side Of Navbar -->--}}
+{{--            <ul class="navbar-nav mr-auto">--}}
 
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('user.create')}}">New Item</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('home')}}">Dashboard</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('contact.us')}}">Contact Us</a>
-                </li>
-
-
-            </ul>
-
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link" href="{{route('user.create')}}">New Item</a>--}}
+{{--                </li>--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link" href="{{route('home')}}">Dashboard</a>--}}
+{{--                </li>--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link" href="{{route('contact.us')}}">Contact Us</a>--}}
+{{--                </li>--}}
 
 
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
+{{--            </ul>--}}
+
+{{--            <!-- Right Side Of Navbar -->--}}
+{{--            <ul class="navbar-nav ml-auto">--}}
+{{--                <!-- Authentication Links -->--}}
+
+
+{{--                @guest--}}
+{{--                    <li class="nav-item">--}}
+{{--                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>--}}
+{{--                    </li>--}}
+{{--                    @if (Route::has('register'))--}}
+{{--                        <li class="nav-item">--}}
+{{--                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>--}}
+{{--                        </li>--}}
+{{--                    @endif--}}
+{{--                @else--}}
+{{--                    <li class="nav-item dropdown">--}}
+{{--                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>--}}
+{{--                            {{ Auth::user()->name }} <span class="caret"></span>--}}
+{{--                        </a>--}}
+
+{{--                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">--}}
+{{--                            <a class="dropdown-item" href="{{ route('logout') }}"--}}
+{{--                               onclick="event.preventDefault();--}}
+{{--                                                     document.getElementById('logout-form').submit();">--}}
+{{--                                {{ __('Logout') }}--}}
+{{--                            </a>--}}
+
+{{--                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
+{{--                                @csrf--}}
+{{--                            </form>--}}
+{{--                        </div>--}}
+{{--                    </li>--}}
+{{--                @endguest--}}
+{{--            </ul>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+
+
+    <div class="bg-white shadow-sm">
+        <div class="container m-auto">
+            <div class="row justify-content-between">
+                <div class="col-3 d-flex">
+                    <div class="border-right border-dark m-auto">
+                        <a href="">
+                            <img src="{{ asset('img/Logo 1.png') }}" alt="">
                         </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
+                    </div>
+                    <h3 class="p-2">BUY<br>SELL <br>STAY AT HOME</h3>
+                </div>
+                <div class="col-7 m-auto">
+                    <div class="d-flex w-75 m-auto">
+                        <div class="w-100 m-auto custom-red b-border-20">
+                            <form action="" class="m-0">
+                                <input type="text" class="w-75 px-3 py-1 b-border-20 border border-danger">
+                                <button class="b-border-20 py-1 border border-danger ml--2 bg-white" type="submit">All Categories</button>
                             </form>
                         </div>
-                    </li>
-                @endguest
-            </ul>
+                        <a class="pl-2" href="">
+                            <img class="w-75 m-auto" src="{{ asset('img/union.png') }}" alt="">
+                        </a>
+                    </div>
+{{--                    <div class="w-75 m-auto">--}}
+{{--                        <ul class="d-inline-flex text-left pl-0">--}}
+{{--                            <li class="border-right px-2"><a class="silver-color" href="">Mask</a></li>--}}
+{{--                            <li class="border-right px-2"><a class="silver-color" href="">Voots</a></li>--}}
+{{--                            <li class="border-right px-2"><a class="silver-color" href="">Boof</a></li>--}}
+{{--                            <li class="border-right px-2"><a class="silver-color" href="">Potato</a></li>--}}
+{{--                            <li class="border-right px-2"><a class="silver-color" href="">Chains</a></li>--}}
+{{--                            <li class="border-right px-2"><a class="silver-color" href="">Switch</a></li>--}}
+{{--                        </ul>--}}
+{{--                    </div>--}}
+                </div>
+                <div class="col-2 m-auto">
+                    <a href="">
+                        <img src="{{ asset('img/Vector.png') }}" alt="">
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
+
+
+
+
+
+
+
+
 </nav>
 
 

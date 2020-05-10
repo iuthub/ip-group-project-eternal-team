16d1f5567eeca914db7f5a6c680f8b903e16f0e5
Mukhammad1999 <m.rustamkhonov@student.inha.uz>
2020-05-10 23:23:44 +0500

    final

--- eternal_project/resources/views/partials/navbar.blade.php
+++ eternal_project/resources/views/partials/navbar.blade.php
@@ -105,6 +105,11 @@
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" v-pre>
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('logout') }}"
